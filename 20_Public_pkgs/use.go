415fdb152e71c45d963ff78a020c4dfffb4569a1
Atharv-K-979 <atharvkulkarni979@gmail.com>
2026-04-14 17:41:29 +0530

    done packages

---
+++ 20_Public_pkgs/use.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/fatih/color"
+)
+
+func main(){
+
+	color.Red("This is a red text")
+	color.Green("This is a green text")
+	color.Blue("This is a blue text")
+
+	
+}
